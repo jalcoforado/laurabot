9b061317e6155e399ac0dad6406039236d0293cb
jalcoforado <jalcoforado@gmail.com>
2020-09-25 12:47:13 -0300

    Jupternovo

--- actions.py
+++ actions.py
@@ -149,3 +149,57 @@ class action_get_governador(Action):
             resposta_final = resposta["Governador"].values[0] + " do partido " + resposta["Partido"].values[0]
             print(str(resposta_final))
             dispatcher.utter_message(str(resposta_final))
+
+
+# especifique o URL
+class action_get_sobrecgm(Action):
+    def name(self):
+        return "action_get_sobrecgm"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        wiki = "http://207.154.234.162/en/home"
+        # Consulte o site e retorne o html para a variável 'page'
+        page = urllib.request.urlopen(wiki)
+        # Parse o html na variável 'page' e armazene-o no formato BeautifulSoup
+        soup = BeautifulSoup(page, "html.parser")
+        # Localiza a table dos nomes do prefeito
+        table = soup.find("table", class_="wikitable sortable")
+        A = []
+        B = []
+        C = []
+        D = []
+        E = []
+
+        for row in table.findAll("tr"):  # para tudo que estiver em <tr>
+            cells = row.findAll("td")  # variável para encontrar <td>
+            if len(cells) == 5:  # número de colunas
+                A.append(cells[0].find(text=True))  # iterando sobre cada linha
+                B.append(str(cells[1].find("a").text).upper())
+                C.append(cells[2].find("a").text)
+                D.append(cells[3].find(text=True))
+                E.append(cells[4].find(text=True))
+
+        df = pd.DataFrame(index=A, columns=["Posição"])
+
+        df["Posição"] = A
+        df["Cidade"] = B
+        df["Estado"] = C
+        df["Prefeito"] = D
+        df["Partido"] = E
+        if Cidade == "none":
+            dispatcher.utter_message(text="Cidade invalida!")
+
+        else:
+            resposta = df[df["Cidade"] == Cidade]
+
+            # resposta = str(resposta)
+            print(resposta)
+            resposta_final = resposta["Prefeito"].values[0] + " do partido " + resposta["Partido"].values[0]
+            print(str(resposta_final))
+            dispatcher.utter_message(str(resposta_final))
